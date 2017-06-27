263235382da4f1a847a72e612419521e33e76308
Robert Puls <rpu1s@sbcglobal.net>
2017-06-27 07:35:10 -0600

    change nav to match others & refactored code

--- js/favorites.js
+++ js/favorites.js
@@ -1,12 +1,12 @@
 $(() => {
-  function addFavoriteImage(homeCoords, hero = false) {
+  let hero = "col s12 amber lighten-1";
+  function addFavoriteImage(homeCoords, hero = "col s5 amber lighten-5") {
     let imgURL = [];
     for (let i = 0; i < 2; i++) {
       console.log(homeCoords[i]);
       imgURL.push(`https://maps.googleapis.com/maps/api/staticmap?maptype=satellite&center=${homeCoords[i].toString()}&zoom=18&size=350x350&key=AIzaSyAiB8Q6zW5qm1u2d5LKrT98udr4wbQKEuk`);
     }
-    if (hero) {
-      $("#favImage").prepend(`<a href="#modal1" style="padding:1%" class="col s12 card-panel amber lighten-1 row valign-wrapper activator">
+      $("#favImage").append(`<a href="#modal1" class="lessImage ${hero} card-panel valign-wrapper activator">
           <div class="col s6">
             <img src="${imgURL[0]}" alt="" class="circle responsive-img favorite-img">
             <p class="red-text">${homeCoords[0]}</p>
@@ -16,18 +16,7 @@ $(() => {
             <p class="red-text">${homeCoords[1]}</p>
           </div>
         </a>`);
-    } else {
-      $("#favImage").append(`<a href="#modal1" class="lessImage col s5 card-panel amber lighten-5 valign-wrapper activator">
-          <div class="col s6">
-            <img src="${imgURL[0]}" alt="" class="circle responsive-img favorite-img">
-            <p class="red-text">${homeCoords[0]}</p>
-          </div>
-          <div class="col s6">
-            <img src="${imgURL[1]}" alt="" class="circle responsive-img favorite-img">
-            <p class="red-text">${homeCoords[1]}</p>
-          </div>
-        </a>`);
-    }
+
     $('.modal').modal();
     $(".activator").on("click", function() {
         console.log(this);
@@ -39,7 +28,7 @@ $(() => {
   addFavoriteImage([
     [39.7578, -105.0072],
     [-44.7584, 192.1819]
-  ], true)
+  ], hero)
   addFavoriteImage([
     [50.7578, -105.0072],
     [-50.7584, 192.1819]
