c1a0f531f7d89ab9a99cfb24bfa0f816315922f0
Robert Puls <rpu1s@sbcglobal.net>
2017-06-27 15:22:31 -0600

    connects signup page to database

---
+++ js/signup.js
@@ -0,0 +1,10 @@
+$(() => {
+  $("#submit").on("click", function () {
+    let newUser = {
+      "name": $("#name-input").val(),
+      "email": $("#email").val(),
+      "password": $("#password").val()
+    };
+    $.post("https://dialocserver-api.herokuapp.com/auth/signup", newUser)
+  });
+});
