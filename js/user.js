e39da94043afe0eb6fced3d8ff9472ff75e3e913
WILL MEIER <willmeierart@gmail.com>
2017-06-28 15:56:44 -0600

    a long days work

--- js/user.js
+++ js/user.js
@@ -2,7 +2,7 @@ $(() => {
   initialize()
 
   function initialize() {
-    const earth = new WE.map('earth_div_markers')
+    const earth = new WE.map('earth_div_markers_user')
     WE.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(earth)
 
     function antipode(coord) {
@@ -12,7 +12,7 @@ $(() => {
     let antip = antipode(coords)
 
     var markerCustom = WE.marker(coords, '/images/bullet_orange.png', 8,8).addTo(earth)
-    var markerCustom2 = WE.marker(antip, '/images/bullet_orange.png', 8,8).addTo(earth)
+    var markerCustom2 = WE.marker(antip, '/images/bullet_pink.png', 8,8).addTo(earth)
 
     earth.setView([39.7578, -105.0072], .8);
   }
